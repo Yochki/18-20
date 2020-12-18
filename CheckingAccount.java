cdef7e8c1dedc636b7600e6ac33abf10b0e3e3b0
Yochki <71455712+Yochki@users.noreply.github.com>
2020-12-18 16:38:59 +0200

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,29 @@
+package second;
+
+public class CheckingAccount extends Account {
+	private double overDraftLimit;
+	
+	public CheckingAccount(int id, double balance, double overDraftLimit){
+		super(id, balance);
+		setOverDraftLimit(overDraftLimit);
+		
+	}
+
+	public double getOverDraftLimit() {
+		return overDraftLimit;
+	}
+
+	public void setOverDraftLimit(double overDraftLimit) {
+		this.overDraftLimit = overDraftLimit;
+	}
+	
+	public void withdraw(double draw) {
+		if(balance + overDraftLimit >= draw) {
+		balance -= draw;
+	}
+	}
+		public String toString() {
+			return super.toString() + "Checking account: ";
+			}
+
+}
